cd6d7640da42240c0cbe173f2408d4ca8f1183bc
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-16 12:02:11 -0600

    add font to text style of theme

---
+++ lib/src/features/editor/utils/monokaiSublimeThemeWithFont.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+
+monokaiSublimeThemeWithFont(String family){
+  final values = monokaiSublimeTheme.values;
+  final keys = monokaiSublimeTheme.keys;
+  final newTheme = Map<String, TextStyle>.fromIterables(keys, values);
+  newTheme.forEach((key, value) {
+    newTheme[key] = value.copyWith(fontFamily: family);
+  });
+  return newTheme;
+}
