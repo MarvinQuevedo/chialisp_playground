a01f393006a00e3f7557615983b264dd7a28cab0
MarvinQuevedo <marvinquevedo2017@outlook.es>
2023-03-17 19:18:37 -0600

    autogenerated files

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_chia_rust_utils/flutter_chia_bls_plugin_c_api.h>
+#include <flutter_chia_rust_utils/flutter_chia_rust_utils_plugin_c_api.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterChiaBlsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterChiaBlsPluginCApi"));
+  FlutterChiaRustUtilsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterChiaRustUtilsPluginCApi"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
