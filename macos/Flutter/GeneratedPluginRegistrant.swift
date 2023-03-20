56d374c1d1d1b823dd19444a0a275d6f8601f139
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-20 13:17:09 -0600

    autogenerated files for macos

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterChiaBlsPlugin.register(with: registry.registrar(forPlugin: "FlutterChiaBlsPlugin"))
+  FlutterChiaRustUtilsPlugin.register(with: registry.registrar(forPlugin: "FlutterChiaRustUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
