7aca5655350e83734bc048c262d828198917c1c2
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-20 19:57:30 -0600

    top open files logic UI

---
+++ lib/src/features/home/presentation/widgets/top_header_project.dart
@@ -0,0 +1,81 @@
+import 'package:chialisp_playground/src/features/editor/providers/projects_handler_provider.dart';
+import 'package:chialisp_playground/src/features/home/presentation/providers/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'hovered_icon_button.dart';
+
+class TopHeaderProject extends StatefulWidget {
+  final ProjectData project;
+  final bool isActive;
+  final Function(ProjectData) onTap;
+  final Function(ProjectData) onClose;
+  final bool modified;
+  const TopHeaderProject({
+    super.key,
+    required this.project,
+    required this.isActive,
+    required this.onTap,
+    required this.onClose,
+    required this.modified,
+  });
+
+  @override
+  State<TopHeaderProject> createState() => _TopHeaderProjectState();
+}
+
+class _TopHeaderProjectState extends State<TopHeaderProject> {
+  bool _hovered = false;
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      
+      
+      child: InkWell(
+        onTap: () {
+          widget.onTap(widget.project);
+        },
+        onHover: (value) {
+          setState(() {
+            _hovered = value;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: !widget.isActive ? (_hovered?Colors.black.withOpacity(0.2):Colors.white.withOpacity(0.05)) : Colors.black38,
+            borderRadius: BorderRadius.circular(0),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          height: 35,
+          child: Row(children: [
+            icon(context),
+            const SizedBox(width: 5),
+            Text(
+              widget.project.fileName,
+              style: TextStyle(
+                color: !widget.isActive ? Colors.white.withOpacity(0.8) : Colors.white,
+              ),
+            ),
+            HoveredIconButton(
+              onPressed: () {
+                widget.onClose(widget.project);
+              },
+              size: 15,
+              icon: Icons.close,
+              hoverColor: ThemeProvider.of(context).hoverColor,
+              
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget icon(BuildContext context) {
+    return SvgPicture.asset(
+      'assets/images/chialisp_dark.svg',
+      width: 20,
+    );
+  }
+}
