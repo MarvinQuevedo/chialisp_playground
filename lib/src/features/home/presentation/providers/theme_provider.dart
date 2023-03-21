2b021c8bd7ad620b92390fee11968c6e1d56bf43
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-20 19:56:42 -0600

    enable theme provider for future changes

--- lib/src/features/home/presentation/providers/theme_provider.dart
+++ lib/src/features/home/presentation/providers/theme_provider.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../styling.dart';
 
-class ThemeProvider extends ChangeNotifier{
+class ThemeProvider extends ChangeNotifier {
   Brightness _brightness = Brightness.dark;
   Brightness get brightness => _brightness;
+
+
   set brightness(Brightness value) {
     _brightness = value;
     notifyListeners();
   }
+
   Color get leftIconsColorDark => AppTheme.leftIconsColorDark;
   Color get leftElementsBackColorDark => AppTheme.leftElementsBackColorDark;
+  Color get hoverColor => Colors.white.withOpacity(0.3);
+  static ThemeProvider of(BuildContext context) {
+    return Provider.of<ThemeProvider>(context);
+  }
 }
