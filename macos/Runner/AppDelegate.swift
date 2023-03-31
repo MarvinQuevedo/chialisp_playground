2843ab273dbc047a7fec688b312dfe559caa12a1
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-31 17:35:16 -0600

    fix call of dummy method

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import flutter_chia_rust_utils
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    FlutterChiaBlsPlugin.dummyMethodToEnforceBundling()
+    FlutterChiaRustUtilsPlugin.dummyMethodToEnforceBundling()
     return true
   }
 }
